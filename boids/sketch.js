ebaced9b54b22e931a85eb0639719a335fe71ea7
Naitik Jain <naitik.jain07@gmail.com>
2025-12-26 18:19:29 -0500

    made orientation and vel work

--- boids/sketch.js
+++ boids/sketch.js
@@ -5,26 +5,56 @@ class Boid {
   constructor(x, y) {
     this.px = x;
     this.py = y;
-    this.vx = 0;
-    this.vy = 0;
-    this.ax = 0;
-    this.ay = 0;
+    this.vx = 1;
+    this.vy = 1;
     this.maxSpeed = 4;
-    this.maxForce = 0.1;
 
-    this.sizex = 10;
-    this.sizey = 20;
+    // check if we need force limits
+    this.Force = 0.1;
+    this.frads = -1;
+
+    this.mass = 1;
+
+    this.sizex = 27;
+    this.sizey = 9;
+
+    this.orientation = 0; // in radians
+
 
   }
 
   draw() {
+
+    push();
+    translate(this.px, this.py);
+    rotate(this.orientation);
     fill(255);
-    quad(50, 62, 86, 50, 50, 38, 14, 50);
+    quad(0, this.sizey,
+      this.sizex, 0,
+      0, -this.sizey,
+      -this.sizex, 0);
+    pop();
+  }
+
+  update () {
+    // Update velocity
+    this.vx += 0;
+    this.vy += 0;
+
+    // see if speed limit is needed
+
+    // Update position
+    this.px += this.vx;
+    this.py += this.vy;
+    // Update orientation to face direction of travel (radians)
+    if (this.vx !== 0 || this.vy !== 0) {
+      this.orientation = Math.atan2(this.vy, this.vx);
+    }
   }
 }
 
 let boids = [];
-let boid1 = new Boid(50, 50);
+let boid1 = new Boid(100, 100);
 
 
 
@@ -35,6 +65,7 @@ function setup() {
 function draw() {
   background(0);
 
+  boid1.update();
   boid1.draw();
 
 }
